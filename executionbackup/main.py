53623fa757625206fb92973f0a6cccca1f62c2db
TennisBowling <tennisbowling@tennisbowling.com>
2022-04-14 07:13:12 -0700

    qrf

--- executionbackup/main.py
+++ executionbackup/main.py
@@ -13,7 +13,7 @@ class ServerOffline(Exception):
 class NodeInstance:
     def __init__(self, url: str):
         self.url: str = url
-        self.session = aiohttp.ClientSession(headers={'Content-type': 'application/json'}, json_serialize=dumps)
+        self.session = aiohttp.ClientSession(json_serialize=dumps)
         self.status: bool = False
         self.dispatch = logger.dispatch
     
@@ -30,9 +30,11 @@ class NodeInstance:
         await self.dispatch('node_offline', self.url)
 
     async def check_alive(self):
+        await self.set_online()
+        return (self, True, 1)
         try:
             start = monotonic()
-            async with self.session.post(self.url, json={'jsonrpc': '2.0', 'method': 'eth_syncing', 'params': [], 'id': 1}, timeout=10) as resp:
+            async with self.session.post(self.url, json={'jsonrpc': '2.0', 'method': 'eth_syncing', 'params': [], 'id': 1}, headers={'Content-type': 'application/json'}, timeout=10) as resp:
                 end = monotonic()
                 if (await resp.json())['result']:
                     await self.set_offline()
@@ -45,10 +47,10 @@ class NodeInstance:
             await self.set_offline()
             return (self, False, ((end - start) * 1000))
     
-    async def do_request(self, data: Dict[str, Any]=None) -> Union[Tuple[str, int, str], ServerOffline]:
+    async def do_request(self, headers: Dict[str, Any], data: Dict[str, Any]=None) -> Union[Tuple[str, int, str], ServerOffline]:
         try:
-            async with self.session.post(self.url, data=data, timeout=3) as resp:   # if we go higher than 3, we may be more counter-productive waiting for all the nodes to respond 
-                return (await resp.text(), resp.status, dumps(dict(resp.headers)))
+            async with self.session.post(self.url, data=data, headers=headers, timeout=3) as resp:   # if we go higher than 3, we may be more counter-productive waiting for all the nodes to respond 
+                return (await resp.text(), resp.status, dict(resp.headers))
         except (aiohttp.ServerTimeoutError, aiohttp.ServerConnectionError, aiohttp.ClientConnectionError, aiohttp.ClientOSError, aiohttp.ClientResponseError):
             await self.set_offline()
             return ServerOffline()
@@ -105,24 +107,21 @@ class NodeRouter:
     async def do_engine_route(self, req: Request) -> None:
         if req.json['method'] == 'engine_getPayloadV1':
             n = await self.get_execution_node()
-            r = await n.do_request(req.body)
-            resp = await req.respond(status=r[1])
-            await resp.send(r[0], end_stream=True)
+            r = await n.do_request(req.headers, req.body)
+            await req.respond(status=r[1]).send(r[0], end_stream=True)
         else:
             # wait for just one node to respond but send it to all
             n = await self.get_execution_node()
-            r = await n.do_request(req.body)
-            resp = await req.respond(status=r[1], headers=loads(r[2]))
-            await resp.send(r[0], end_stream=True)
-            [asyncio.create_task(node.do_request(req.body)) for node in self.nodes if node.status and node != n]
+            r = await n.do_request(req.headers, req.body)
+            await req.respond(status=r[1], headers=r[2]).send(r[0], end_stream=True)
+            [asyncio.create_task(node.do_request(req.headers, req.body)) for node in self.nodes if node.status and node != n]
             return
     
     async def route(self, req: Request) -> None:
         # handle "normal" requests
         n = await self.get_execution_node()
-        r = await n.do_request(req.body)
-        resp = await req.respond(status=r[1], headers=loads(r[2]))
-        await resp.send(r[0], end_stream=True)
+        r = await n.do_request(req.headers, req.body)
+        await req.respond(status=r[1], headers=r[2]).send(r[0], end_stream=True)
             
 
     async def stop(self) -> None:
