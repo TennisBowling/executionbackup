e1d6aaa41743b2af8906c9f10ba9ee01e57202a3
TennisBowling <tennisbowling@tennisbowling.com>
2022-04-27 06:53:59 -0700

    wip: rust

--- executionbackup-rust/src/main.rs
+++ executionbackup-rust/src/main.rs
@@ -5,6 +5,8 @@ use reqwest::{
     Client,
 };
 use std::time::Instant;
+use tracing::{debug, info, warn, Level};
+use tracing_subscriber::fmt;
 
 struct NodeInstance {
     pub url: String,
@@ -21,8 +23,19 @@ impl NodeInstance {
         }
     }
 
+    pub async fn set_alive(&mut self) {
+        self.status = true;
+        info!(url = %self.url, "Node is alive");
+    }
+
+    pub async fn set_offline(&mut self) {
+        self.status = false;
+        info!("Node {} is offline", self.url);
+    }
+
     pub async fn check_alive(&mut self) -> Result<(bool, u128), Box<dyn std::error::Error>> {
         let start = Instant::now();
+
         let r = json::parse(
             &self
                 .do_request(
@@ -31,10 +44,11 @@ impl NodeInstance {
                 )
                 .await?,
         )?;
+
         let end = start.elapsed();
 
         if r["result"] == false {
-            self.status = true;
+            self.set_alive().await;
         }
 
         Ok((self.status, end.as_millis()))
@@ -54,7 +68,27 @@ impl NodeInstance {
             );
         }
 
-        let r = &self
+        // send request and if successful return the response. Otherwise return an error and set status to false
+        let response = self
+            .client
+            .post(&self.url)
+            .headers(map)
+            .body(data)
+            .send()
+            .await?;
+
+        if response.status().is_success() {
+            Ok(response.text().await?)
+        } else {
+            self.status = false;
+            Err(Box::new(std::io::Error::new(
+                std::io::ErrorKind::Other,
+                "Request failed",
+            )))
+        }
+
+        /*
+        let r = self
             .client
             .post(&self.url)
             .body(data)
@@ -62,14 +96,21 @@ impl NodeInstance {
             .send()
             .await?
             .text()
-            .await?;
-
-        Ok(r.to_string())
+            .await?; */
     }
 }
 
 #[tokio::main]
 async fn main() {
+    let format = fmt::format().with_target(false);
+    let subscriber = fmt()
+        .event_format(format)
+        .with_max_level(Level::INFO)
+        .finish();
+
+    tracing::subscriber::set_global_default(subscriber)
+        .expect("Setting default subscriber failed. File an issue at https://github.com/TennisBowling/executionbackup/issues");
+
     let mut node: NodeInstance = NodeInstance::new(String::from("http://192.168.86.83:8545"));
     let res = node.check_alive().await;
     match res {
