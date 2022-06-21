77c26fdba50edce253091d9c7c467d41d8fa3904
TennisBowling <tennisbowling@tennisbowling.com>
2022-06-21 14:19:41 -0700

    correct versions & release

--- util.hpp
+++ util.hpp
@@ -82,7 +82,7 @@ boost::program_options::variables_map parse_args(int argc, char *argv[])
 
     if (vm.count("version"))
     {
-        std::cout << "executionbackup-cpp version 0.1 BETA\n";
+        std::cout << "executionbackup-cpp version 0.1.0 BETA\n";
         std::cout << "Compiled with " << BOOST_COMPILER << std::endl;
         exit(0);
     }
