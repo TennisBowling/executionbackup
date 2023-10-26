24bcbfb8f8051a42b48e353b4ec60176d7231465
TennisBowling <tennisbowling@tennisbowling.com>
2023-10-25 19:40:54 -0700

    v1.1.2

--- src/main.rs
+++ src/main.rs
@@ -21,7 +21,7 @@ mod verify_hash;
 use types::ExecutionPayload;
 use verify_hash::verify_payload_block_hash;
 
-const VERSION: &str = "1.1.1";
+const VERSION: &str = "1.1.2";
 const DEFAULT_ALGORITHM: jsonwebtoken::Algorithm = jsonwebtoken::Algorithm::HS256;
 
 lazy_static! {
