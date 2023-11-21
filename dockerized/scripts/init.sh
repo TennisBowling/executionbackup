473f5fc5d668079ed0a7d782291cdf0e0de0b678
cipiasentini <cipiasentini@gmail.com>
2023-11-21 10:50:50 -0300

    flag for only updating docker-compose.yml

--- dockerized/scripts/init.sh
+++ dockerized/scripts/init.sh
@@ -1,4 +1,11 @@
 #!/bin/bash
 
 echo $JWT_SECRET > /scripts/.jwt
-executionbackup -p $PORT -n $NODES --listen-addr 0.0.0.0 --jwt-secret /scripts/.jwt --log-level $LOG_LEVEL
+executionbackup \
+-p $PORT \
+-n $NODES \
+--listen-addr 0.0.0.0 \
+--jwt-secret /scripts/.jwt \
+--log-level $LOG_LEVEL \
+--fcu-majority $FCU_MAJORITY \
+--node-timings
