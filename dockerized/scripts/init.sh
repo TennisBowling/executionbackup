5f4a665e5dc5ca5c4b0b90bdf914c33c871ff9c4
carloscip <98537833+carloscip@users.noreply.github.com>
2023-12-01 21:07:42 -0300

    Docker Feature/release tag (#29)
    * simplified for docker build paths
    
    * flag for only updating docker-compose.yml
    
    * fixed suggestions

--- dockerized/scripts/init.sh
+++ dockerized/scripts/init.sh
@@ -1,4 +1,17 @@
 #!/bin/bash
 
+START_PARAMS=""
+
+if [ "$SHOW_NODE_TIMINGS" = "true" ]; then
+  START_PARAMS="$START_PARAMS --node-timings "
+fi
+
 echo $JWT_SECRET > /scripts/.jwt
-executionbackup -p $PORT -n $NODES --listen-addr 0.0.0.0 --jwt-secret /scripts/.jwt --log-level $LOG_LEVEL
+executionbackup \
+$START_PARAMS \
+-p $PORT \
+-n $NODES \
+--listen-addr 0.0.0.0 \
+--jwt-secret /scripts/.jwt \
+--log-level $LOG_LEVEL \
+--fcu-majority $FCU_MAJORITY
